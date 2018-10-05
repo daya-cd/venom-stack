3a28115664a22108d9765153055f1d175118e2d2
dayanithi <dayanithi.hari@atkinsglobal.com>
2018-10-05 17:16:04 +0100

    updated transactions

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const api = require('./api')
+const api = require('./api')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
